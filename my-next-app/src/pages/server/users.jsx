11d0e771ff9178ad3637fab6f89cc88ac47f7a05
kiran04-code <kr551344@gmail.com>
2025-07-06 11:08:16 +0530

    nextLearingday000000000002

--- my-next-app/src/pages/server/users.jsx
+++ my-next-app/src/pages/server/users.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import style from "../../styles/User.module.css"
 const user = (props) => {
   return (
     <div>
       <h1>Hello From Server!(SSR)</h1>
       {
         props.datas.users && props.datas.users.map((items)=>
-          <div className='bg-red-500'>
+          <div className={style.text} >
             <Link href={ `/server/users/${items.id}`}>{items.firstName}</Link>
             </div>
         )
@@ -16,7 +17,7 @@ const user = (props) => {
 }
 export const getServerSideProps = async()=>{
   const data = await  fetch("https://dummyjson.com/users")
-  const datas = await data.json()
+  const datas = await data.json()  
   return{
     props:{
       datas
